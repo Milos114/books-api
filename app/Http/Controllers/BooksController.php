0f644414888258c8a983b9685c6284e675d92c2d
Milos Stefanovic <mstefanovic40@gmail.com>
2017-01-08 22:48:29 +0100

    book change

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -51,8 +51,12 @@ class BooksController extends Controller
     {
         $book = Book::create($request->all());
 
-        return response()->json(['created' => true], 201, [
-            'Location' => route('books.show', ['id' => $book->id])
+        return response()->json([
+            'created' => true
+        ], 201, [
+            'Location' => route('books.show', [
+                'id' => $book->id
+            ])
         ]);
     }
 
